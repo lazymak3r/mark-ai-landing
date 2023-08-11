350d68ecdd250e04d44bf15f0c33be0e54bf6186
Aram <aramsuqiasyan20@gmail.com>
2023-08-11 14:47:50 +0400

    feat: add process section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import styles from './page.module.scss'
 import {Hero} from "@/components/Hero/Hero";
 import {Header} from "@/components/Header/Header";
 import {ValueProposition} from "@/components/ValueProposition/ValueProposition";
+import {Process} from "@/components/Process/Process";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Header/>
       <Hero/>
       <ValueProposition/>
+      <Process/>
     </main>
   )
 }
---
+++ src/components/Process/Process.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+
+import classes from './Process.module.scss'
+import classNames from "classnames";
+
+interface ProcessProps {
+
+}
+
+export const Process: React.FC<ProcessProps> = (props) => {
+  return (
+    <div className={classes.process}>
+      <div className="container">
+        <div className={classes.processSectionTitleWrapper}>
+          <h2 className={classes.processSectionTitle}>process zohatic has follow</h2>
+          <p className={classes.processSectionParagraphText}>
+            We have become a preferred partner to the leading biofuel refiners, major oil companies, blenders,
+            retailers, and other commodity company.
+          </p>
+        </div>
+        <div className={classes.processWrapper}>
+          <div className={classes.processGrid}>
+            <div className={classes.processSingleWrapper}>
+              <div className={classNames([classes.processNumberWrapper, classes.processNumberWrapper1])}>
+                <div className={classes.processNumber}>1</div>
+              </div>
+              <h3 className={classes.processTitle}>Create your account</h3>
+              <p className={classes.processDescriptionText}>
+                Bust down data silos by connecting all of your data – &nbsp;then build powerful segment family brand
+                ambassadors
+              </p>
+            </div>
+            <div className={classes.processSingleWrapper}>
+              <div className={classNames([classes.processNumberWrapper, classes.processNumberWrapper2])}>
+                <div className={classes.processNumber}>2</div>
+              </div>
+              <h3 className={classes.processTitle}>Create your account</h3>
+              <p className={classes.processDescriptionText}>
+                Bust down data silos by connecting all of your data – &nbsp;then build powerful segment family brand
+                ambassadors
+              </p>
+            </div>
+            <div className={classes.processSingleWrapper}>
+              <div className={classNames([classes.processNumberWrapper, classes.processNumberWrapper3])}>
+                <div className={classes.processNumber}>3</div>
+              </div>
+              <h3 className={classes.processTitle}>Create your account</h3>
+              <p className={classes.processDescriptionText}>
+                Bust down
+                data silos by connecting all of your data – &nbsp;then build powerful segment family brand
+                ambassadors </p>
+            </div>
+          </div>
+          <img
+            src="https://assets.website-files.com/6455ed9f0d3281b2f933d00d/645a49337b0a306ac4165ecf_Process%20Arrow1.png"
+            loading="lazy" alt="" className={classes.processArrowImage1}
+          />
+          <img
+            src="https://assets.website-files.com/6455ed9f0d3281b2f933d00d/645a49336a368fb58addf629_Process%20Arrow2.png"
+            loading="lazy" alt="" className={classes.processArrowImage2}
+          />
+          {/*<div className="process-line" style="will-change: width, height; width: 39.043%; height: 1px;"></div>*/}
+        </div>
+      </div>
+    </div>
+  );
+};
