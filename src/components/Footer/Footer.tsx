2d94bf816746b70d664989615d2a87ae86cad552
Aram <aramsuqiasyan20@gmail.com>
2023-08-14 16:15:17 +0400

    feat: add Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,9 @@ export const Footer: React.FC<FooterProps> = (props) => {
   return (
     <div className={classes.footer}>
       <div className={'container'}>
-
+        <div className={classes.inner}>
+          © 2023 AI Startup Generator. All Rights Reserved
+        </div>
       </div>
     </div>
   );
